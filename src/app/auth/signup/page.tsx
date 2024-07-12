c851a0a5ad6cf23025fbfeaebfabcb96a44dfa55
stepheeeen <stephenon56@gmail.com>
2024-07-12 22:47:48 +0100

    mehnn

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -4,7 +4,7 @@ import DefaultImage from '../../../../public/assets/images/DefaultImage.png'
 
 const SignUp = () => {
   return (
-    <AuthContainer src={DefaultImage}>
+    <AuthContainer src={DefaultImage} title='Create a new account' text='Safe and Secure. We will never share your data.'>
         may
     </AuthContainer>
   )
--- src/components/auth/Container.tsx
+++ src/components/auth/Container.tsx
@@ -1,13 +1,15 @@
 import React, { ReactNode } from 'react';
 import Image, { StaticImageData } from 'next/image';
 
-const AuthContainer = ({ src, children }: { src: StaticImageData | string; children: ReactNode }) => {
+const AuthContainer = ({ src, children, title, text }: { src: StaticImageData | string; children: ReactNode; title: string; text: string; }) => {
   return (
     <div className="w-[100%] flex items-center">
       <div className="w-[47%] flex items-center p-2">
         <Image src={src} alt="authentication image" className='' />
       </div>
-      <div className="w-[53%] flex justify-center items-center">
+      <div className="w-[53%] flex justify-center items-center flex-col">
+        <h1 className=''>{title}</h1>
+        <p>{text}</p>
         <form>
           {children}
         </form>
