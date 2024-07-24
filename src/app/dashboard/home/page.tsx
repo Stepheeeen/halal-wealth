d5b7b59adee0e5f0400e6e35f0046d1774d938f8
stepheeeen <stephenon56@gmail.com>
2024-07-24 02:14:31 +0100

    02:14

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,12 +1,28 @@
+"use client"
 import React from 'react';
+import { useState } from 'react';
 import DashboardContainer from '@/components/dashboard/dashboardContainer';
+import { BalanceCard } from '@/components/reusable/card/Card';
+import {FundWalletIcon, HideIconWhite, ShowIconWhite, WithdrawIcon } from '../../../../public/assets/icons';
 
 const Page = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
   return (
     <DashboardContainer
-    PageTItle='Dashboard'
+      PageTItle='Dashboard'
     >
-        card
+      <div className="w-full overflow-x-auto mt-2 whitespace-nowrap bg-white shadow-sm rounded-lg p-4">
+        <div className="inline-flex space-x-4 bg-white">
+          <BalanceCard customStyle='bg-[#4E05DC]' styleName='walletBg' CardTitle='Wallet Balance' hideBalance={handleClick} BalanceIcon={show ? <HideIconWhite/> : <ShowIconWhite/>} Balance={show? '₦ 100,000' : '****'} handleClick1={''} handleClick2={''} button1='Fund wallet' button2='Withdraw'  buttonIcon1={<FundWalletIcon />} buttonIcon2={<WithdrawIcon/>} styling1='bg-[#14013A]' styling2='bg-[#fff] text-[#8046F2]'/>
+          <BalanceCard customStyle='bg-[#14013A]' styleName='' CardTitle='Savings Balance' hideBalance={handleClick} BalanceIcon={show ? <HideIconWhite/> : <ShowIconWhite/>} Balance={show? '₦ 100,000' : '****'} handleClick1={''} handleClick2={''} button1='See Plans' button2='Create new plan' buttonIcon1={''} buttonIcon2={''} styling1='bg-[#fff] text-[#14013A]' styling2='bg-[#fff] text-[#14013A]'/>
+          <BalanceCard customStyle='bg-[#14013A]' styleName='' CardTitle='Investment Balance' hideBalance={handleClick} BalanceIcon={show ? <HideIconWhite/> : <ShowIconWhite/>} Balance={show? '₦ 100,000' : '****'} handleClick1={''} handleClick2={''} button1='See Plans' button2='Create new plan'  buttonIcon1={''} buttonIcon2={''} styling1='bg-[#fff] text-[#14013A]' styling2='bg-[#fff] text-[#14013A]'/>
+        </div>
+      </div>
+
+      <div>
+
+      </div>
     </DashboardContainer>
   );
 };
--- src/components/reusable/card/Card.tsx
+++ src/components/reusable/card/Card.tsx
@@ -1,4 +1,5 @@
 import React,{ReactNode} from 'react'
+import { Button, ButtonSpinner } from '@chakra-ui/react';
 
 const Card = ({children, cardStyle}:{children:ReactNode; cardStyle: string;}) => {
   return (
@@ -8,4 +9,29 @@ const Card = ({children, cardStyle}:{children:ReactNode; cardStyle: string;}) =>
   )
 }
 
-export default Card
+const BalanceCard = ({customStyle, styleName, CardTitle, hideBalance, BalanceIcon, Balance, handleClick1, handleClick2, button1, button2, buttonIcon1, buttonIcon2, styling1, styling2}:{customStyle: any; styleName: string;CardTitle: string; hideBalance: any; BalanceIcon: any; Balance: string; handleClick1: any; handleClick2: any; button1: string; button2: string; buttonIcon1: any; buttonIcon2: any; styling1: string; styling2: string;}) => {
+  return (
+    <div className={`w-[425px] rounded-lg h-[190px] p-5 text-[#fff] mr-[5px] ${customStyle} ${styleName}`}>
+      <div className='flex items-center'>
+        <p>{CardTitle}</p>
+        <Button onClick={hideBalance} className='ml-[15px]'>
+            {BalanceIcon}
+        </Button>
+      </div>
+      <div>
+        <h1 className='font-[600] text-[40px] mb-3'>{Balance}</h1>
+      </div>
+      <div>
+        <Button variant='solid' className={`px-3 py-2 rounded font-[500] ${styling1}`} onClick={handleClick1}>
+            {button1} <span> {buttonIcon1}</span>
+        </Button>
+        <Button variant='solid' className={`px-3 py-2 rounded ml-3 font-[500] ${styling2}`} onClick={handleClick2}>
+            {button2} <span>{buttonIcon2}</span>
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+
+export {Card, BalanceCard}
