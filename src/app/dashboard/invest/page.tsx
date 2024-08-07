e4f5824c204d16426087d5485bdcb4e780274ca4
steph4n <stephenon56@gmail.com>
2024-08-07 01:02:42 -0700

    error still contiunues

--- src/app/dashboard/invest/page.tsx
+++ src/app/dashboard/invest/page.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react'
 import DashboardContainer from '@/components/dashboard/dashboardContainer'
 
-const InvestContainer = ({children}: {children:ReactNode}) => {
+const InvestContainer = ({children}: {children: ReactNode;}) => {
   return (
     <DashboardContainer PageTItle='Invest'>
       <div className="w-full overflow-x-auto mt-2 whitespace-nowrap bg-white shadow-sm rounded-lg p-3">
