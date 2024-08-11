4ba2f7007f40f551f33e0c48796137420a0a583f
stepheeeen <stephenon56@gmail.com>
2024-08-11 04:49:11 +0100

    mr money vibess

--- src/app/dashboard/savings/page.tsx
+++ src/app/dashboard/savings/page.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import DashboardContainer from '@/components/dashboard/dashboardContainer'
+import { HideIcon, ShowIcon } from '../../../../public/assets/icons'
+import { BalanceCard } from '@/components/reusable/card/Card'
 
 const page = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
   return (
-    <div>
-      
-    </div>
+    <DashboardContainer PageTItle='Save'>
+      <BalanceCard customStyle='text-black p-0 ml-[-10px]' styleName='' CardTitle='Investment Total' hideBalance={handleClick} BalanceIcon={show ? <HideIcon /> : <ShowIcon />} Balance={show ? '₦ 100,000' : '****'} handleClick1={''} handleClick2={''} button1='' button2='' buttonIcon1={''} buttonIcon2={''} styling1='' styling2='' />
+    </DashboardContainer>
   )
 }
 
