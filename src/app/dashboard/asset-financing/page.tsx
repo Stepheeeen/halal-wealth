c46cecbf082963f47e82406c7935ab775ff11eee
stepheeeen <stephenon56@gmail.com>
2024-08-24 00:17:58 +0100

    liked modal

--- src/app/dashboard/asset-financing/page.tsx
+++ src/app/dashboard/asset-financing/page.tsx
@@ -15,36 +15,46 @@ const page = () => {
   const [isOpen, setIsOpen] = useState(false);
   const handleOpen = () => setIsOpen(true);
   const handleClose = () => setIsOpen(false);
-  
-  
+
+  const [Details, setDetails] = useState(false);
+  const handleDetailsOpen = () => setDetails(true);
+  const handleDetailsClose = () => setDetails(false);
+
+
   const handleTabsChange = (index: any) => {
     setSelectedIndex(index);
   };
-  
+
   const selectBillers = [
     {
       value: "PES",
       label: "PES",
     },
   ];
-  
+
   const favourite = [
-    { Src: PesLight },
-    { Src: PesBlack },
-    { Src: PesLight },
-    { Src: PesBlack },
-    { Src: PesLight },
-    { Src: PesBlack },
-    { Src: PesLight },
+    { Src: PesLight, productName: 'Sony playstation 5 (PS5)', productDownPrice: '100,000', price: '500,000', },
+    { Src: PesBlack, productName: 'Xbox-Series-X-Black-Edition', productDownPrice: '100,000', price: '500,000', },
+    { Src: PesLight, productName: 'Sony playstation 5 (PS5)', productDownPrice: '100,000', price: '500,000', },
+    { Src: PesBlack, productName: 'Xbox-Series-X-Black-Edition', productDownPrice: '100,000', price: '500,000', },
+    { Src: PesLight, productName: 'Sony playstation 5 (PS5)', productDownPrice: '100,000', price: '500,000', },
+    { Src: PesBlack, productName: 'Xbox-Series-X-Black-Edition', productDownPrice: '100,000', price: '500,000', },
+    { Src: PesLight, productName: 'Sony playstation 5 (PS5)', productDownPrice: '100,000', price: '500,000', },
+    { Src: PesBlack, productName: 'Xbox-Series-X-Black-Edition', productDownPrice: '100,000', price: '500,000', },
   ]
-  
+
+  const favouriteLiked = [
+    { Src: PesLight, productName: 'Sony playstation 5 (PS5)', productDownPrice: '100,000', price: '500,000', },
+    { Src: PesBlack, productName: 'Xbox-Series-X-Black-Edition', productDownPrice: '100,000', price: '500,000', },
+  ]
+
   const [liked, setLiked] = useState<boolean[]>(Array(favourite.length).fill(false));
   const toggleLike = (index: number) => {
     // Toggle the liked state for the specific card
     const updatedLikedStates = liked.map((liked, i) => (i === index ? !liked : liked));
     setLiked(updatedLikedStates);
   };
-  
+
   return (
     <DashboardContainer PageTItle='Asset financing'>
       <div className='bg-[#F9FAFB]'>
@@ -74,17 +84,20 @@ const page = () => {
 
                 <div className='grid grid-cols-4 gap-1 ml-2 py-2'>
                   {favourite.map((card, i) => (
-                    <div>
-                    <DefaultCard cardStyle='bg-[#F2F4F7] w-[220px] h-[245px] p-2 rounded-xl relative grid place-items-center mb-2' key={i}>
-                      <div className='absolute top-2 right-2 cursor-pointer' onClick={() => toggleLike(i)}>
-                        {liked[i] ?
-                          <LikedIconPurple />
-                          :
-                          <LikedIcon />
-                        }
-                      </div>
-                      <Image alt='asset categories' src={card.Src} className='w-[150px] h-[160px]'/>
-                    </DefaultCard>
+                    <div onClick={handleDetailsOpen} key={i} className='mb-3'>
+                      <DefaultCard cardStyle='bg-[#F2F4F7] w-[220px] h-[245px] p-2 rounded-xl relative grid place-items-center mb-2'>
+                        <div className='absolute top-2 right-2 cursor-pointer' onClick={() => toggleLike(i)}>
+                          {liked[i] ?
+                            <LikedIconPurple />
+                            :
+                            <LikedIcon />
+                          }
+                        </div>
+                        <Image alt='asset categories' src={card.Src} className='w-[150px] h-[160px]' />
+                      </DefaultCard>
+                      <h1 className='text-[#14013A] font-[500] text-[16px]'>{card.productName}</h1>
+                      <p className='text-[#5C556C] font-[470] text-[15px]'>Down payment: NGN {card.productDownPrice}</p>
+                      <h1 className='text-[#8046F2] font-[570] text-[17px]'>NGN {card.price}</h1>
                     </div>
                   ))}
 
@@ -98,7 +111,30 @@ const page = () => {
         </div>
       </div>
 
-      <CustomModal ModalStyling='' modalTitle='Liked' isOpen={isOpen} onClose={handleClose} >
+      <CustomModal ModalStyling='overflow-scroll p-0' modalTitle='Liked' isOpen={isOpen} onClose={handleClose} >
+        <div className='grid grid-cols-2 gap-3 ml-2 py-2'>
+          {favouriteLiked.map((card, i) => (
+            <div key={i} className='mb-3'>
+              <DefaultCard cardStyle='bg-[#F2F4F7] h-[186px] p-2 rounded-xl relative grid place-items-center mb-2'>
+                <div className='absolute top-2 right-2 cursor-pointer' onClick={() => toggleLike(i)}>
+                  {liked[i] ?
+                    <LikedIcon />
+                    :
+                    <LikedIconPurple />
+                  }
+                </div>
+                <Image alt='asset categories' src={card.Src} className='w-[110px]' />
+              </DefaultCard>
+              <h1 className='text-[#14013A] font-[450] text-[13px]'>{card.productName}</h1>
+              <p className='text-[#5C556C] font-[400] text-[12px]'>Down payment: NGN {card.productDownPrice}</p>
+              <h1 className='text-[#8046F2] font-[470] text-[14px]'>NGN {card.price}</h1>
+            </div>
+          ))}
+
+        </div>
+      </CustomModal>
+
+      <CustomModal ModalStyling='' modalTitle='Details' isOpen={Details} onClose={handleDetailsClose} >
         hello
       </CustomModal>
     </DashboardContainer>
