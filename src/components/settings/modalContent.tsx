5e960b1620328fb7210ef70043ab1ad0037b30be
stepheeeen <stephenon56@gmail.com>
2024-09-03 07:42:35 +0100

    settings-correction

--- src/components/settings/modalContent.tsx
+++ src/components/settings/modalContent.tsx
@@ -10,7 +10,7 @@ const Card = ({Title, children, height}: {Title: string, children: ReactNode, he
   return(
     <div>
     <h1 className='text-[17px] font-[550] mb-2 text-[#14013A]'>{Title}</h1>
-    <div className={`bg-white shadow-sm rounded-sm mb-3 p-2 h-[${height}] `}>
+    <div className={`bg-white shadow-sm rounded-sm mb-3 p-2 h-[${height}] h-[320px]`}>
       {children}
     </div>
     </div>
